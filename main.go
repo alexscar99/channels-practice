31912cee4f5e26bb093bf60c271c70232cf56218
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-21 05:06:21 -0400

    Refactored for loop to continuously wait 5 sec, then create new goroutine for http request func

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -16,23 +17,30 @@ func main() {
 
 	c := make(chan string)
 
+	// create new child goroutine for each link
 	for _, link := range links {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	// wait for chan to return some value, assign to l, and then iterate
+	// pause for 5 seconds before creating new checkLink goroutine to not spam site
+	for l := range c {
+		time.Sleep(5 * time.Second)
+		go checkLink(l, c)
 	}
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
+
+	// if error, send status error msg through chan
 	if err != nil {
-		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		fmt.Println("Error:", link, "may currently be down.")
+		c <- link
 		return
 	}
 
-	fmt.Println(link, "is up!")
-	c <- "Yep it's up"
+	// send status success msg through chan
+	fmt.Println(link, "is up and visible!")
+	c <- link
 }
